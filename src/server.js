0d69427cf50c6c4e919e556d4d524b5b0599b404
amartello <lunafreelancerr@gmail.com>
2026-01-31 00:13:22 -0300

    conecto  MongoDB

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 4000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
